ee46ee80c57c691df10f05f3603157d94270be7d
Simon Verhoeven <simon.verhoeven@telepathy.ai>
2022-06-24 19:54:40 +0800

    fix and update

--- packages/web-app/src/trpc.ts
+++ packages/web-app/src/trpc.ts
@@ -1,4 +1,4 @@
 import { createReactQueryHooks } from '@trpc/react';
-import { AppRouter } from 'api-server';
+import { AppRouter } from '@gsg-code-assignment/api-server';
 
 export const trpc = createReactQueryHooks<AppRouter>();
