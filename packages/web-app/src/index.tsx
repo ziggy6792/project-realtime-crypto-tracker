d5df613a6b347979bc58228a9e646a90553146bc
Simon Verhoeven <simon.verhoeven@telepathy.ai>
2022-06-27 22:25:10 +0800

    rename

--- packages/web-app/src/index.tsx
+++ packages/web-app/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './app-2';
+import App from './app';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
