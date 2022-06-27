2a3ef1c03b67774e4cdebe53223cb3d31918f818
Simon Verhoeven <simon.verhoeven@telepathy.ai>
2022-06-27 23:49:39 +0800

    backend test setup

--- packages/api-server/jest.config.js
+++ packages/api-server/jest.config.js
@@ -1,18 +1,13 @@
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable import/order */
 /* eslint-disable @typescript-eslint/no-var-requires */
-const tsconfig = require('./tsconfig.json');
-const base = require('../../jest.config.base.js');
 const package = require('./package.json');
+const base = require('../../jest.config.base.js');
 
 module.exports = {
   ...base,
   displayName: package.name,
   roots: ['<rootDir>/src'],
   moduleDirectories: ['node_modules', '<rootDir>'],
-  moduleNameMapper: {
-    '\\.(css|scss)$': 'identity-obj-proxy',
-  },
-  setupFilesAfterEnv: ['./src/utils/setup-tests.ts'],
-  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['./src/test-utils/setup-after-env.ts'],
 };
---
+++ packages/api-server/src/index.test.ts
@@ -0,0 +1,5 @@
+describe('Score Run', () => {
+  it('score big comp', async () => {
+    expect(1 + 1).toEqual(2);
+  });
+});
