868f96a0b6effac15c98fbacb7e7400f549f6991
Simon Verhoeven <simon.verhoeven@telepathy.ai>
2022-06-28 04:45:07 +0800

    cleanup

--- packages/web-app/src/test-utils/setup-tests.ts
+++ packages/web-app/src/test-utils/setup-tests.ts
@@ -7,43 +7,8 @@
 import '@testing-library/jest-dom';
 // For async tests
 import 'regenerator-runtime/runtime';
-import { WebSocket, Server } from 'mock-socket';
 
 require('jest-fetch-mock').enableMocks();
 
 process.env.REACT_APP_API_GSG_INTERNAL_URL = 'http://mock/api';
-process.env.REACT_APP_API_GSG_INTERNAL_WS_URL = 'ws://localhost:1234';
-
-/*
- * By default the global WebSocket object is stubbed out when
- * a new Server instance is created and is restored when you stop
- * the server.
- * However, you can disable this behavior by passing `mock: false`
- * to the options and manually mock the socket when you need it.
- */
-// const server = new Server(process.env.REACT_APP_API_GSG_INTERNAL_WS_URL, { mock: true });
-
-// server.on('connection', (socket) => {
-//   console.log('connection');
-//   socket.on('message', (bla) => {
-//     console.log('message', bla);
-//     server.emit('onUpdatePrice', { id: 4, result: { type: 'data', data: { json: { fromSymbol: 'BTC', toSymbol: 'USD', ammount: 20701.07 } } } });
-//     return { id: 4, result: { type: 'data', data: { json: { fromSymbol: 'BTC', toSymbol: 'USD', ammount: 20701.07 } } } };
-//     // socket.send(
-//     //   JSON.stringify({
-//     //     result: {
-//     //       type: 'started',
-//     //     },
-//     //     elapsedMs: 11,
-//     //     context: {},
-//     //   })
-//     // );
-//     // socket.send(JSON.stringify({ id: 4, result: { type: 'data', data: { json: { fromSymbol: 'BTC', toSymbol: 'USD', ammount: 20701.07 } } } }));
-//   });
-//   socket.send(JSON.stringify({ id: 4, result: { type: 'data', data: { json: { fromSymbol: 'BTC', toSymbol: 'USD', ammount: 20701.07 } } } }));
-
-//   socket.on('close', () => {});
-
-// });
-
-// window.WebSocket = WebSocket; // Here we stub out the window object
+process.env.REACT_APP_API_GSG_INTERNAL_WS_URL = 'ws://mock/websocket';
