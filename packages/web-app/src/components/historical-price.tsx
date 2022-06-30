9c36cfab0f446c5617f84f549256374546f12dac
Simon Verhoeven <simon.verhoeven@telepathy.ai>
2022-06-30 20:45:53 +0800

    fix ticks

--- packages/web-app/src/components/historical-price.tsx
+++ packages/web-app/src/components/historical-price.tsx
@@ -3,6 +3,7 @@ import { trpc } from 'src/trpc';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Label } from 'recharts';
 import { format } from 'date-fns';
 import { FromSymbol } from '@gsg-code-assignment/api-server';
+import _ from 'lodash';
 
 interface IHistoricalPriceProps {
   fromSymbol: FromSymbol;
@@ -20,18 +21,24 @@ const HistoricalPriceChart: React.FC<IHistoricalPriceProps> = ({ fromSymbol }) =
     return [];
   }, [data]);
 
+  // https://github.com/recharts/recharts/issues/953#issuecomment-506772381
+  const { min, max } = useMemo(
+    () => ({ min: _.min(data?.map((d) => d.price.ammount)) as number, max: _.max(data?.map((d) => d.price.ammount)) as number as number }),
+    [data]
+  );
+
   if (error) return <div>{error.message}</div>;
 
   return (
-    <LineChart width={1200} height={300} data={chartData} margin={{ top: 5, right: 20, bottom: 25, left: 40 }}>
+    <LineChart width={1200} height={300} data={chartData} margin={{ top: 5, right: 20, bottom: 25, left: 60 }}>
       <Line type='monotone' dataKey='price' stroke='#8884d8' dot={false} />
       <XAxis dataKey='hour' dy={8}>
         <Label value='Hour' position='bottom' offset={10} />
       </XAxis>
       <CartesianGrid strokeDasharray='3 3' />
       <Tooltip />
-      <YAxis dataKey='price' dx={-6} type='number' domain={['dataMin', 'dataMax']} tickCount={3} tickFormatter={(tick) => tick.toLocaleString()}>
-        <Label value='Price in USD' position='left' angle={-90} style={{ textAnchor: 'middle' }} offset={20} />
+      <YAxis dataKey='price' dx={-6} type='number' domain={['dataMin', 'dataMax']} ticks={[min, max]} tickFormatter={(tick) => tick.toLocaleString()}>
+        <Label value='Price in USD' position='left' angle={-90} style={{ textAnchor: 'middle' }} offset={40} />
       </YAxis>
     </LineChart>
   );
