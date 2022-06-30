fdbd71533c9a4cdfc0c473c26313bca0fecfe362
Simon Verhoeven <simon.verhoeven@telepathy.ai>
2022-06-30 14:15:58 +0800

    split app routers

--- packages/api-server/src/app-router.ts
+++ packages/api-server/src/app-router.ts
@@ -1,37 +1,14 @@
-import * as trpc from '@trpc/server';
 import superjson from 'superjson';
-import { ee } from './utils/event-emiter';
-import * as cryptocompareService from './services/cryptocompare-api';
-import { getHisoricalDataRequestValidator } from './validators';
-import { Price } from './domain-models/price';
-import { getHisoricalDataResponseToHistoricalPrices } from './utils/mapper-util';
+import { createRouter } from './createRouter';
+import { historicalPriceRouter } from './routers/get-historical-price';
+import { updatePriceRouter } from './routers/on-update-price';
 
-export const appRouter = trpc
-  .router()
-  .transformer(superjson)
-  .subscription('onUpdatePrice', {
-    resolve() {
-      // `resolve()` is triggered for each client when they start subscribing `onUpdatePrice`
-
-      // return a `Subscription` with a callback which is triggered immediately
-      return new trpc.Subscription<Price>((emit) => {
-        const onUpdate = (data: Price) => emit.data(data);
-
-        // trigger `onUpdate()` when `updatePrice` is triggered in our event emitter
-        ee.on('updatePrice', onUpdate);
-
-        // unsubscribe function when client disconnects or stops subscribing
-        return () => {
-          ee.off('updatePrice', onUpdate);
-        };
-      });
+export const appRouter = createRouter()
+  .query('helloWorld', {
+    async resolve() {
+      return 'helloWorld!';
     },
   })
-  .query('getHistoricalPrice', {
-    input: getHisoricalDataRequestValidator,
-    async resolve({ input }) {
-      const apiResponse = await cryptocompareService.getHisoricalData(input);
-      const historicalPrices = getHisoricalDataResponseToHistoricalPrices(apiResponse, input.fromSymbol, input.toSymbol);
-      return historicalPrices;
-    },
-  });
+  .transformer(superjson)
+  .merge(historicalPriceRouter)
+  .merge(updatePriceRouter);
